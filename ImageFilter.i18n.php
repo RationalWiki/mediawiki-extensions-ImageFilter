7ff34197ccb0ea80139ed5460c4beef952e69c95
Jack Phoenix <jack@countervandalism.net>
2017-07-01 18:21:21 +0300

    Coding style tweaks
    No functional changes here.

--- ImageFilter.i18n.php
+++ ImageFilter.i18n.php
@@ -2,6 +2,6 @@
 $messages = array();
 
 $messages['en'] = array(
-  'tog-displayfiltered'            => 'Display filtered images',
-  'nsfw-warning'                   => '<sup>Warning: NSFW Image!</sup>',
+	'tog-displayfiltered'            => 'Display filtered images',
+	'nsfw-warning'                   => '<sup>Warning: NSFW Image!</sup>',
 );
--- ImageFilter.php
+++ ImageFilter.php
@@ -19,70 +19,79 @@ $wgHooks['ImageBeforeProduceHTML'][] = 'ImageFilterProduceHTML';
 $wgHooks['ArticleSaveComplete'][] = 'ImageFilterUpdateCache';
 $wgHooks['GetPreferences'][] = 'ImageFilterPreferences';
 
-function ImageFilterPreferences( $user, &$preferences )
-{
+function ImageFilterPreferences( $user, &$preferences ) {
 	$preferences['displayfiltered'] = array(
 		'type' => 'toggle',
 		'label-message' => 'tog-displayfiltered',
 		'section' => 'rendering/files',
 	);
- 
+
 	return true;
 }
 
-function ImageFilterHash( $hash ) 
-{
+function ImageFilterHash( $hash ) {
 	global $wgUser;
 	$hash .= '!' . ( $wgUser->getOption( 'displayfiltered' ) ? '1' : '' );
 	return true;
 }
 
-function ImageFilterProduceHTML( &$skin, &$title, &$file, &$frameParams, &$handlerParams, &$time, &$res ) 
-{
+function ImageFilterProduceHTML( &$skin, &$title, &$file, &$frameParams, &$handlerParams, &$time, &$res ) {
 	global $wgUser;
-	if ($wgUser->getOption( 'displayfiltered' )) return true;
-	/*getDescriptionText parses the text ( and it screws up the parser), so we have to do it manually*/
+
+	if ( $wgUser->getOption( 'displayfiltered' ) ) {
+		return true;
+	}
+
+	/* getDescriptionText parses the text (and it screws up the parser), so we
+	have to do it manually */
 	$revision = Revision::newFromTitle( $title );
-  if ( !$revision ) return true;
-  $text = $revision->getText();
-  if ( !$text ) return true;
-	if ( strpos($text,'__NSFW__') === FALSE ) {
+	if ( !$revision ) {
+		return true;
+	}
+
+	$text = $revision->getText();
+	if ( !$text ) {
+		return true;
+	}
+
+	if ( strpos( $text, '__NSFW__' ) === false ) {
 		return true;
 	} else {
-		if ($frameParams['caption'] !== '') {
-			$res = $skin->link($title,$frameParams['caption']);
+		if ( $frameParams['caption'] !== '' ) {
+			$res = $skin->link( $title, $frameParams['caption'] );
 		} else {
-			$res = $skin->link($title);
+			$res = $skin->link( $title );
 		}
-		$res .= wfMessage('nsfw-warning')->text();
+		$res .= wfMessage( 'nsfw-warning' )->text();
 		return false;
 	}
 }
 
-function ImageFilterUpdateCache( &$article, &$user, $text, $summary, $minoredit, $watchthis, $sectionanchor, &$flags, $revision, &$status )
-{
-		$title = $article->getTitle();
-		//no change recorded
-		if ($revision == null) return true;
-		if ( $title->getNamespace() == NS_FILE ) {
-			$prevrevision = $revision->getPrevious();
-			if ($prevrevision == null) {
-				$prevflagged = false;
-			} else {
-				$prevflagged = strpos($prevrevision->getRawText(),'__NSFW__') !== FALSE;
-			}
-			$curflagged = strpos($revision->getRawText(),'__NSFW__') !== FALSE;
-			if ($prevflagged XOR $curflagged) {
-				# Invalidate cache for all pages using this file
-				$update = new HTMLCacheUpdate( $title, 'imagelinks' );
+function ImageFilterUpdateCache( &$article, &$user, $text, $summary, $minoredit, $watchthis, $sectionanchor, &$flags, $revision, &$status ) {
+	$title = $article->getTitle();
+	// no change recorded
+	if ( $revision == null ) {
+		return true;
+	}
+	if ( $title->getNamespace() == NS_FILE ) {
+		$prevRevision = $revision->getPrevious();
+		if ( $prevRevision == null ) {
+			$prevFlagged = false;
+		} else {
+			$prevFlagged = strpos( $prevRevision->getRawText(), '__NSFW__' ) !== false;
+		}
+		$curFlagged = strpos( $revision->getRawText(), '__NSFW__' ) !== false;
+		if ( $prevFlagged xor $curFlagged ) {
+			# Invalidate cache for all pages using this file
+			$update = new HTMLCacheUpdate( $title, 'imagelinks' );
+			$update->doUpdate();
+			# Invalidate cache for all pages that redirects on this page
+			$redirs = $title->getRedirectsHere();
+			foreach ( $redirs as $redir ) {
+				$update = new HTMLCacheUpdate( $redir, 'imagelinks' );
 				$update->doUpdate();
-				# Invalidate cache for all pages that redirects on this page
-				$redirs = $title->getRedirectsHere();
-				foreach( $redirs as $redir ) {
-					$update = new HTMLCacheUpdate( $redir, 'imagelinks' );
-					$update->doUpdate();
-				}
 			}
 		}
-		return true;
+	}
+	return true;
 }
